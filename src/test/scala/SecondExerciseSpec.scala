658ab0c733fe542172ae37c02cbb3a6e9893fdca
Carlos Daniel Cañon Carrero <daniel.canon@redb.ee>
2019-06-22 17:03:43 -0300

    refactor elem method and update tests

--- src/test/scala/SecondExerciseSpec.scala
+++ src/test/scala/SecondExerciseSpec.scala
@@ -1,26 +1,29 @@
-import SecondExercise.FoldRightWrapper
+import SecondExercise.FoldRightContainer
 import org.scalatest._
 
 class SecondExerciseSpec extends FlatSpec with Matchers with OptionValues with Inside with Inspectors {
 
   val seq: scala.collection.Seq[Int] = 1 to 10
-  val subject: FoldRightWrapper      = new FoldRightWrapper(seq)
+  val subject: FoldRightContainer    = new FoldRightContainer(seq)
 
-  it should "sum using foldRight" in {
+  it should "sum must be equals to Seq.sum" in {
     assert(subject.sum == seq.sum)
   }
 
-  it should "elem should find using foldRight" in {
+  it should "elem get same element inside seq" in {
     assert(subject.elem(3) == Option(seq(3)))
-    assert(subject.elem(5) == Option(seq(5)))
   }
 
-  it should "filter distinct should using foldRight" in {
+  it should "elem return None when index is out" in {
+    assert(subject.elem(11).isEmpty)
+  }
+
+  it should "filter works equals to Seq.filterNot" in {
     val elements = Seq(1, 5, 6)
     assert(subject.filter(elements) == seq.filterNot(elements.contains))
   }
 
-  it should "map like seq.filter" in {
+  it should "map works like Seq.map" in {
     @inline def mapFunction(int: Int) = int.toString + "#"
     assert(subject.map(mapFunction) == seq.map(mapFunction))
   }
