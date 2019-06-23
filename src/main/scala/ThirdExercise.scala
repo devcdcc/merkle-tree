198e8180a34bb6195cfe0c18543188bdf9ccef55
Carlos Daniel Cañon Carrero <daniel.canon@redb.ee>
2019-06-23 20:18:56 -0300

    refactor test and implement simple first case

--- src/main/scala/ThirdExercise.scala
+++ src/main/scala/ThirdExercise.scala
@@ -1,7 +1,14 @@
 import scala.collection.mutable
 
-class ThirdExercise(seq: mutable.Seq[Int]) {
-  def score: Int = ???
+class ThirdExercise {
+  private val seq = mutable.ListBuffer.empty[Int]
+
+  def roll(pins: Int*) = seq.addAll(pins)
+
+  def score: Int = {
+    val out = seq.sum
+    out
+  }
 
   //
 }
--- src/test/scala/ThirdExerciseSpec.scala
+++ src/test/scala/ThirdExerciseSpec.scala
@@ -3,16 +3,15 @@ import scala.collection.mutable
 
 class ThirdExerciseSpec extends FlatSpec with Matchers with OptionValues with Inside with Inspectors {
 
-  val seq     = mutable.Seq.empty[Int]
-  var subject = new ThirdExercise(seq)
+  var subject = new ThirdExercise()
   it should "a" in {
-
     //given
+    subject.roll(10, 5, 5)
 
     //when
     val score: Int = subject.score
 
     //then
-    assert(score == 30)
+    assert(score == 20)
   }
 }
