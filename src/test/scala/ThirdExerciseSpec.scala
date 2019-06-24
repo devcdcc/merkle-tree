2843b5a14c6d4211ceaa5828477344928894b46c
Carlos Daniel Cañon Carrero <daniel.canon@redb.ee>
2019-06-23 22:15:06 -0300

    update test, added first fraame

--- src/test/scala/ThirdExerciseSpec.scala
+++ src/test/scala/ThirdExerciseSpec.scala
@@ -1,14 +1,18 @@
 import org.scalatest._
-import scala.collection.mutable
 
 class ThirdExerciseSpec extends FlatSpec with Matchers with OptionValues with Inside with Inspectors {
 
   var subject = new ThirdExercise()
-  it should "a" in {
+
+  val strike01 = Seq(10)
+  val spare02  = Seq(5, 5)
+  val open03   = Seq(9, 0)
+  it should "return 20 in first case" in {
     //given
-    subject.roll(10, 5, 5)
+    val total1: Seq[Int] = strike01 :+ spare02
 
     //when
+    subject.roll(total1: _*)
     val score: Int = subject.score
 
     //then
